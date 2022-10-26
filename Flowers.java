576d736259fa09068659b31b3a0879021dbf92a0
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2022-10-26 23:11:28 +0800

    Update Flowers.java

--- Flowers.java
+++ Flowers.java
@@ -1,7 +1,4 @@
 public class Flowers { 
-    private int turnips;
-    private int rose;
-    private int sunflower;
     private int turnipsSellPrice = 5;
     private int roseSellPrice = 9;
     private int sunflowerSellPrice = 19;
@@ -10,33 +7,6 @@ public class Flowers {
     private int sunflowerBuyPrice = 20;
 
     public Flowers() {
-        this.turnips = 0;
-        this.rose = 0;
-        this.sunflower = 0;
-    }
-
-    public int getTurnips() {
-        return this.turnips;
-    }
-
-    public void setTurnips(int amount) {
-        this.turnips += amount;
-    }
-
-    public int getRose() {
-        return this.rose;
-    }
-
-    public void setRose(int amount) {
-        this.rose += amount;
-    }
-
-    public int getSunflower() {
-        return this.sunflower;
-    }
-
-    public void setSunflower(int amount) {
-        this.sunflower += amount;
     }
 
     public int getTurnipsSellPrice() {
