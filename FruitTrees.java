fd32d6613cc772e4f9bac89775439d3135931c15
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2022-10-26 23:11:45 +0800

    Update FruitTrees.java

--- FruitTrees.java
+++ FruitTrees.java
@@ -1,30 +1,10 @@
 public class FruitTrees {
-    private int mango;
-    private int apple;
     private int mangoSellPrice = 8;
     private int appleSellPrice = 5;
     private int mangoBuyPrice = 100;
     private int appleBuyPrice = 200;
 
     public FruitTrees() {
-        this.mango = 0;
-        this.apple = 0;
-    }
-
-    public int getMango() {
-        return this.mango;
-    }
-
-    public void setMango(int amount) {
-        this.mango += amount;
-    }
-
-    public int getApple() {
-        return this.apple;
-    }
-
-    public void setApple(int amount) {
-        this.apple += amount;
     }
 
     public int getMangoSellPrice() {
