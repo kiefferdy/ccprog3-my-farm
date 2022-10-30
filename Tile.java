77d7bdb685f5fc5fae4a8de1193a1515baf3eace
John Kieffer Recato Dy <115262644+kiefferdy@users.noreply.github.com>
2022-10-30 19:17:56 +0800

    Methods complete!

--- Tile.java
+++ Tile.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Tile {
     private int xPos, yPos;
-    private boolean hasRock;
     private boolean isPlowed;
+    private boolean hasRock;
+    private boolean hasCrop;
     private Crop crop;
     private ArrayList<Crop> cropList;
     private int daysToHarvest;
@@ -12,10 +16,29 @@ public class Tile {
     public Tile(int xPos, int yPos) {
         this.xPos = xPos;
         this.yPos = yPos;
+        this.isPlowed = false;
+        this.hasCrop = false;
     }
     
     public void displayStatus() {
-        
+        System.out.printf("Status of Tile %d, %d", this.xPos, this.yPos);
+        System.out.printf("\n\tHas Been Plowed: %s", this.isPlowed ? "Yes" : "No");
+        System.out.printf("\n\tContains Rock: %s", this.hasRock ? "Yes" : "No");
+        System.out.printf("\n\tContains Withered Crop: %s", this.hasWitheredCrop ? "Yes" : "No");
+        System.out.printf("\n\tCrop Planted: %s", this.hasCrop ? this.crop.getName() : "N/A");
+        System.out.printf("\n\tTime Until Harvest: %d", this.hasCrop ? this.daysToHarvest : "N/A");
+        System.out.printf("\n\tRemaining Water Needs: %d", this.hasCrop ? this.crop.getWaterNeeds() - this.timesWatered : "N/A");
+    }
+    
+    public void addToCropList(Crop crop) {
+        int count;
+        for(Crop existingCrop : cropList) {
+            if(existingCrop.getName().equals(crop.getName())) {
+                return false;
+            }
+        }
+        cropList.add(crop);
+        return true;
     }
     
     public ArrayList<Crop> getCropList() {
@@ -23,9 +46,21 @@ public class Tile {
     }
     
     public boolean plantCrop(Crop crop) {
-        boolean result = false;
-        
-        return result;
+        if(this.crop != null) {
+            return false;
+        }
+        this.crop = crop;
+        this.daysToHarvest = crop.getHarvestTime();
+        this.timesWatered = 0;
+        this.timesFertilized = 0;
+        return true;
+    }
+    
+    public void ageCrop() {
+        if(daysToHarvest <= 0) {
+            this.hasWitheredCrop = true;
+        }
+        this.daysToHarvest--;
     }
     
     public Crop getCrop() {
@@ -33,37 +68,56 @@ public class Tile {
     }
     
     public boolean hasRock() {
-        boolean result = false;
-        
-        return result;
+        return this.hasRock;
     }
     
-    public void setRock() {
-           
+    public void setRock(boolean hasRock) {
+        this.hasRock = hasRock;
     }
     
-    public boolean useShovel() {
-        boolean result = false;
-        
-        return result;
+    public void useShovel() {
+        this.hasWitheredCrop = false;
+        this.crop = null;
+        this.timesWatered = 0;
+        this.timesFertilized = 0;
     }
     
-    public boolean usePickaxe() {
-        boolean result = false;
-        
-        return result;
+    public void usePickaxe() {
+        this.hasRock = false;
     }
     
     public boolean Water() {
-        boolean result = false;
-        
-        return result;
+        if(this.hasWitheredCrop) {
+            System.out.println("You cannot water a withered crop!");
+            return false;
+        }
+        this.timesWatered++;
+        return true;
     }
     
     public boolean Fertilize() {
-        boolean result = false;
+        if(this.hasWitheredCrop) {
+            System.out.println("You cannot fertilize a withered crop!");
+            return false;
+        }
+        this.timesFertilized++;
+        return true;
+    }
+    
+    public int Harvest() {
+        if(this.hasWitheredCrop) {
+            System.out.println("You cannot harvest a withered crop!");
+            return -1;
+        }
+        if(this.daysToHarvest > 0) {
+            System.out.printf("You need to wait %d day(s) to harvest this crop.\n", this.daysToHarvest);
+            return -1;
+        }
         
-        return result;
+        Random rand = new Random();
+        int produce = rand.nextInt(crop.getMaxProduce() - crop.getMinProduce() + 1);
+        produce += crop.getMinProduce();
+        return produce;
     }
     
     public int getXPos() {
