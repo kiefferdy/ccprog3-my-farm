9acbee5dfca12bf0dd2771cc84715c88c868f5ef
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2022-10-30 14:22:49 +0800

    Update MyFarm.java

--- MyFarm.java
+++ MyFarm.java
@@ -12,12 +12,44 @@ public class MyFarm {
     this.farmer = new Farmer();
     this.landWidth = 1;
     this.landLength = 1;
-    this.land = new Tile[landWidth][landLength];
+    this.land = new Tile[landLength][landWidth];
     this.rocks = 0;
     this.days = 1;
     this.harvestable = 0;
     this.withered = 0;
   }
   
+  public void displayLand() {
+    
+  }
   
+  public int getRocks() {
+    
+  }
+  
+  public int getHarvestable() {
+    
+  }
+  
+  public int getWithered() {
+    
+  }
+  
+  public int checkDay() {
+    return this.day; 
+  }
+  
+  public void nextDay() {
+    this.day++; 
+  }
+  
+  public boolean checkGameOver() {
+    boolean result = false;
+    
+    if(getWithered() + getRocks() == 50) {
+      result = true; 
+    }
+    
+    return result;
+  }
 }
