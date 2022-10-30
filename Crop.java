b8e3b5b052fdea14062f06d30a6aa7b835636218
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2022-10-30 15:13:36 +0800

    Update Crop.java

--- Crop.java
+++ Crop.java
@@ -1,3 +1,77 @@
 public class Crop {
+    private String name;
+    private String type;
+    private int harvestTime;
+    private int waterNeeds;
+    private int waterBonusLimit;
+    private int fertilizerNeeds;
+    private int fertilizerBonusLimit;
+    private int minProduce;
+    private int maxProduce;
+    private int seedCost;
+    private int baseHarvestPrice;
+    private int expYield;
     
+    public Crop(String name, String type, int harvestTime, int waterNeeds, int waterBonusLimit, int fertilizerNeeds, int fertilizerBonusLimit, int minProduce, int maxProduce, int seedCost, int baseHarvestPrice, int expYield) {
+        this.name = name;
+        this.type = type;
+        this.harvestTime = harvestTime;
+        this.waterNeeds = waterNeeds;
+        this.waterBonusLimit = waterBonusLimit;
+        this.fertilizerNeeds = fertilizerNeeds;
+        this.fertilizerBonusLimit = fertilizerBonusLimit;
+        this.minProduce = minProduce;
+        this.maxProduce = maxProduce;
+        this.seedCost = seedCost;
+        this.baseHarvestPrice = baseHarvestPrice;
+        this.expYield = expYield;
+    }
+    
+    public String getName() {
+        return this.name;   
+    }
+    
+    public String getType() {
+        return this.type;   
+    }
+    
+    public int getHarvestTime() {
+        return this.harvestTime;   
+    }
+    
+    public int getWaterNeeds() {
+        return this.waterNeeds;   
+    }
+    
+    public int getWaterBonusLimit() {
+        return this.waterBonusLimit;   
+    }
+    
+    public int getFertilizerNeeds() {
+        return this.fertilizerNeeds;   
+    }
+    
+    public int getFertilizerBonusLimit() {
+        return this.fertilizerBonusLimit;   
+    }
+    
+    public int getMinProduce() {
+        return this.minProduce;   
+    }
+    
+    public int getMaxProduce() {
+        return this.maxProduce;   
+    }
+    
+    public int getSeedCost() {
+        return this.seedCost;   
+    }
+    
+    publc int getBaseHarvestPrice() {
+        return this.baseHarvestPrice;
+    }
+    
+    public int getEXPYield() {
+        return this.expYield;   
+    }
 }
