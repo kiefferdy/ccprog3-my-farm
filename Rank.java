5f36ae48f157a47a3c5c4b042ae6c6731b53d52d
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2022-11-07 17:13:59 +0800

    Added Javadoc Comments

--- Rank.java
+++ Rank.java
@@ -1,3 +1,6 @@
+/**
+ * This class represents the rank of the player
+ */
 public class Rank {
     private String name;
     private int levelRequirements;
@@ -7,6 +10,16 @@ public class Rank {
     private int bonusFertilizerLimit;
     private int registrationFee;
     
+    /**
+     * This constructor shows the details of a rank
+     * @param name is the name of the rank
+     * @param levelRequirements is the level needed for the player to reach the rank
+     * @param bonusEarnings is the bonus earnings that the player will get when reaching the rank
+     * @param costReduction is the cost reduction that the player will get when reaching the rank
+     * @param bonusWaterLimit is the bonus water limit that the player will get when reaching the rank
+     * @param bonusFertilizerLimit is the bonus fertilizer limit that the player will get when reaching the rank
+     * @param registrationFee is the registration fee the player needs to get the rank
+     */
     public Rank(String name, int levelRequirements, int bonusEarnings, int costReduction, int bonusWaterLimit, int bonusFertilizerLimit, int registrationFee) {
         this.name = name;
         this.levelRequirements = levelRequirements;
@@ -17,30 +30,58 @@ public class Rank {
         this.registrationFee = registrationFee;
     }
     
+    /**
+     * This method gets the name of the rank
+     * @return the name of the rank
+     */
     public String getRank() {
         return this.name;   
     }
     
+    /**
+     * This method gets the level needed for the player to reach the rank
+     * @return the level requirement of the player to reach the rank
+     */
     public int getLevelRequirements() {
         return this.levelRequirements;   
     }
     
+     /**
+     * This method gets the bonus earnings that the player will get when reaching the rank
+     * @return the bonus earnings the player will get when reaching the rank
+     */
     public int getBonusEarnings() {
         return this.bonusEarnings;   
     }
     
+    /**
+     * This method gets the cost reduction that the player will get when reaching the rank
+     * @return the cost reduction the player will get when reaching the rank
+     */
     public int getCostReduction() {
         return this.costReduction;   
     }
-        
+    
+     /**
+     * This method gets the bonus water limit that the player will get when reaching the rank
+     * @return the bonus water limit the player will get when reaching the rank
+     */
     public int getBonusWaterLimit() {
         return this.bonusWaterLimit;   
     }
     
+    /**
+     * This method gets the bonus fertilizer limit that the player will get when reaching the rank
+     * @return the bonus fertilizer limit the player will get when reaching the rank
+     */
     public int getBonusFertilizerLimit() {
         return this.bonusFertilizerLimit;   
     }
     
+    /**
+     * This method gets the registration fee the player needs to get the rank
+     * @return the registration fee the player needs to get the rank
+     */
     public int getRegistrationFee() {
         return this.registrationFee;   
     }
