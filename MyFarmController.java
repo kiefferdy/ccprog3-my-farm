905bd38ef927ba046f14d36bb1a1afb24c0031a6
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2022-12-01 15:04:13 +0800

    Update MyFarmController.java

--- MyFarmController.java
+++ MyFarmController.java
@@ -1,3 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
+
 public class MyFarmController {
+  private MyFarmView view;
+  private MyFarmModel model;
   
+  public MyFarmController(MyFarmView view, MyFarmModel model) {
+    this.view = view;
+    this.model = model;
+    
+    // add decision making code here
+  }
 }
