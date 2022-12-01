bb42e4d0c2b135f3d7ef55c4b7e59558080afa28
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2022-12-01 15:01:38 +0800

    Update MyFarmView.java

--- MyFarmView.java
+++ MyFarmView.java
@@ -1,4 +1,14 @@
+import java.swing.JFrame;
+import java.swing.JLabel;
+import java.swing.JButton;
+import java.swing.JTextField;
+import java.swing.JPanel;
+
 public class MyFarmView {
-  
+   private JFrame main;
+   
+  public MyFarmView() {
+    
+  }
 }
 
