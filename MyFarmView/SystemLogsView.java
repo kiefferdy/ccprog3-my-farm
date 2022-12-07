969ff0a35edc998a8619374dc1d51c812216e218
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2022-12-07 21:58:12 +0800

    Update SystemLogsView.java

--- MyFarmView/SystemLogsView.java
+++ MyFarmView/SystemLogsView.java
@@ -1,11 +1,12 @@
 package MyFarmView;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.border.LineBorder;
 
 public class SystemLogsView extends JPanel{
     private JTextPane logs;
@@ -14,14 +15,13 @@ public class SystemLogsView extends JPanel{
         this.logs = new JTextPane();
         this.logs.setFont(new Font("Cambria", Font.PLAIN, 16));
         this.logs.setEditable(false);
+        this.logs.setPreferredSize(new Dimension(750, 75));
         this.logs.setAlignmentY(CENTER_ALIGNMENT);
+        this.logs.setBorder(new LineBorder(Color.BLACK));
         this.logs.setText("Welcome to Farming Simulator Rip-Off! Enjoy the game!");
 
-        JScrollPane scrollPane = new JScrollPane(this.logs, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setPreferredSize(new Dimension(750, 90));
-
         this.setAlignmentY(CENTER_ALIGNMENT);
-        this.add(scrollPane);
+        this.add(this.logs);
     }
 
     public JTextPane getLogs() {
